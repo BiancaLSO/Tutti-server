94309c1d2bfd1d5a1dc59d2c378e40ba04d6e649
Maria Otelea <otelea.lavi@gmail.comm>
2022-11-08 16:46:16 +0100

    save encrypted password

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,15 +3,15 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from 'src/dto/create-user.dto';
 import { User, UserDocument } from 'src/schemas/user.schema';
+import { encodePassword } from 'src/utils/bcrypt';
+
+
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  //   async create(createCatDto: CreateCatDto): Promise<Cat> {
-  //     const createdCat = new this.catModel(createCatDto);
-  //     return createdCat.save();
-  //   }
+ 
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
@@ -21,8 +21,11 @@ export class UserService {
     return this.userModel.findOne({ username: username }).exec();
   }
   create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+   const password =  encodePassword(createUserDto.password);
+   console.log(password);
+    const createdUser = new this.userModel({ ...createUserDto, password });
     return createdUser.save();
   }
+ 
 
 }
